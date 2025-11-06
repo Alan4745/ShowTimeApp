2e542c2728e5de88a53346f0a369407313e55dc4
Alan4745 <alaneduardodelacruz407@gmail.com>
2025-11-06 02:34:31 -0600

    agregmoas imagnes

--- src/data/learnCategories.ts
+++ src/data/learnCategories.ts
@@ -3,95 +3,114 @@ export const learnCategories = [
   {
     key: 'training',
     subcategories: [
-        { key: 'strength', 
-            image: require('../../assets/img/learn/training/workout.png'),        
-        },
-        { key: 'power', 
-            image: require('../../assets/img/learn/training/gymflow.png'),            
-        },
-        { key: '4weekProgram', 
-            image: require('../../assets/img/learn/training/recovery.png'),            
-        },
-        { key: "darwinsExerciseLibrary", 
-            image: require('../../assets/img/learn/training/gymflow.png'),            
-        },
-        
+      {
+        key: 'strength',
+        image: require('../../assets/img/learn/training/workout.png'),
+      },
+      {
+        key: 'power',
+        image: require('../../assets/img/learn/training/gymflow.png'),
+      },
+      {
+        key: '4weekProgram',
+        image: require('../../assets/img/learn/training/recovery.png'),
+      },
+      {
+        key: 'darwinsExerciseLibrary',
+        image: require('../../assets/img/learn/training/darwin_exercises.png'),
+      },
     ],
-  },  
+  },
   {
     key: 'mindset',
     subcategories: [
-        { key: 'motivation', 
-            image: require('../../assets/img/learn/mindset/motivation.png'),            
-        },
-        { key: 'selfConfidence', 
-            image: require('../../assets/img/learn/mindset/routine_habits.png'),            
-        },
-        { key: 'mindfulness', 
-            image: require('../../assets/img/learn/mindset/focus_discipline.png'),            
-        },
-        { key: 'focus', 
-            image: require('../../assets/img/learn/mindset/confidence.png'),            
-        },
-        { key: 'visualization', 
-            image: require('../../assets/img/learn/mindset/focus_discipline.png'),            
-        },
-        
+      {
+        key: 'motivation',
+        image: require('../../assets/img/learn/mindset/motivation.png'),
+      },
+      {
+        key: 'selfConfidence',
+        image: require('../../assets/img/learn/mindset/routine_habits.png'),
+      },
+      {
+        key: 'mindfulness',
+        image: require('../../assets/img/learn/mindset/mindfulness.png'),
+      },
+      {
+        key: 'focus',
+        image: require('../../assets/img/learn/mindset/focus.png'),
+      },
+      {
+        key: 'visualization',
+        image: require('../../assets/img/learn/mindset/visualization.png'),
+      },
     ],
-  },  
+  },
   {
     key: 'nutrition',
     subcategories: [
-        { key: 'carbohydrates', 
-            image: require('../../assets/img/learn/nutrition/fuel.png'),            
-        },
-        { key: 'protein', 
-            image: require('../../assets/img/learn/nutrition/hydration.png'),            
-        },
-        { key: 'fat', 
-            image: require('../../assets/img/learn/nutrition/supplements.png'),            
-        },
-        { key: 'supplementsHydration', 
-            image: require('../../assets/img/learn/nutrition/recovery_meals.png'),            
-        },
-        { key: '4WeekPlan', 
-            image: require('../../assets/img/learn/nutrition/supplements.png'),            
-        },
-        
+      {
+        key: 'carbohydrates',
+        image: require('../../assets/img/learn/nutrition/carbohydrates.png'),
+      },
+      {
+        key: 'protein',
+        image: require('../../assets/img/learn/nutrition/protein.png'),
+      },
+      {
+        key: 'fat',
+        image: require('../../assets/img/learn/nutrition/fat.png'),
+      },
+      {
+        key: 'supplementsHydration',
+        image: require('../../assets/img/learn/nutrition/supplements.png'),
+      },
+      {
+        key: '4WeekPlan',
+        image: require('../../assets/img/learn/nutrition/4_week_program.png'),
+      },
     ],
-  },  
+  },
   {
     key: 'tactics',
     subcategories: [
-        { key: 'finishing', 
-            image: require('../../assets/img/learn/tactics/game_analysis.png'),            
-        },
-        { key: 'technical', 
-            image: require('../../assets/img/learn/tactics/positioning.png'),            
-        },
-        { key: 'passingDribbling', 
-            image: require('../../assets/img/learn/tactics/common_mistakes.png'),            
-        }      
+      {
+        key: 'finishing',
+        image: require('../../assets/img/learn/tactics/Finishing.png'),
+      },
+      {
+        key: 'technical',
+        image: require('../../assets/img/learn/tactics/Technical.png'),
+      },
+      {
+        key: 'passingDribbling',
+        image: require('../../assets/img/learn/tactics/Passing_Dribbling.png'),
+      },
     ],
-  },  
+  },
   {
     key: 'gameAnalysis',
     subcategories: [
-        { key: 'defenders', 
-            image: require('../../assets/img/learn/tactics/game_analysis.png'),            
-        },
-        { key: 'forwards', 
-            image: require('../../assets/img/learn/tactics/positioning.png'),            
-        },
-        { key: 'generalAnalysis', 
-            image: require('../../assets/img/learn/tactics/common_mistakes.png'),            
-        },      
-        { key: 'interactive', 
-            image: require('../../assets/img/learn/nutrition/hydration.png'),            
-        },     
-        { key: 'midfielders', 
-            image: require('../../assets/img/learn/tactics/common_mistakes.png'),            
-        }
+      {
+        key: 'defenders',
+        image: require('../../assets/img/learn/game_analysis/defenders.png'),
+      },
+      {
+        key: 'forwards',
+        image: require('../../assets/img/learn/game_analysis/forwards.png'),
+      },
+      {
+        key: 'generalAnalysis',
+        image: require('../../assets/img/learn/game_analysis/generalAnalysis.png'),
+      },
+      {
+        key: 'interactive',
+        image: require('../../assets/img/learn/game_analysis/interactive.png'),
+      },
+      {
+        key: 'midfielders',
+        image: require('../../assets/img/learn/game_analysis/midfielders.png'),
+      },
     ],
-  },  
+  },
 ];
